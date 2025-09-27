aa76f49bfa1035ad6281f6f0ef9e30b6dbcb22ab
savio-alexandre <alexandresaviosouza@gmail.com>
2025-09-27 13:00:47 -0300

    adicionando arquivos aula005

---
+++ fundamentos/aula005/tipagemFraca.js
@@ -0,0 +1,13 @@
+/**
+ * O Javascript é uma linguagem fracamente típada, 
+ * isso significa que  “ele” tentará converter em tempo
+ * de execução os dados no valor. 
+ */
+
+let qualquer = "Legal!";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
